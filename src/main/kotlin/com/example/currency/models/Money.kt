6679b9adb3d6a602f69f074ffd564d3896398090
hrg921 <hrg921@gmail.com>
2019-12-31 09:35:25 +0900

    refactor: use factory methods for Dollar and Franc

--- src/main/kotlin/com/example/currency/models/Money.kt
+++ src/main/kotlin/com/example/currency/models/Money.kt
@@ -1,6 +1,16 @@
 package com.example.currency.models
 
 abstract class Money(open val amount: Int) {
+    companion object {
+        fun dollar(amount: Int): Dollar {
+            return Dollar(amount)
+        }
+
+        fun franc(amount: Int): Franc {
+            return Franc(amount)
+        }
+    }
+
     final override fun equals(other: Any?): Boolean {
         print(other)
         if (other !is Money) {
